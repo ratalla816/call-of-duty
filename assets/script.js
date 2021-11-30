231bccce85ada24bda9bec1fdbf0e37e2353cbb3
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-11-29 21:57:16 -0600

    MVP achieved!

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,33 @@
 
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+const character = document.getElementById("character");
+const block = document.getElementById("block");
 
 function jump() {
-  character.classList.add("animate");
+    // if (character.classList.contains("jump")) {
+    //     return;
+    // } 
+  if (character.classList !="animate") {
+    character.classList.add("animate");
+    }
     setTimeout(function() {
       character.classList.remove("animate");
     }, 500);
 }
+
+const checkDead = setInterval(function() {
+    const characterTop = 
+    parseInt(window.getComputedStyle(character)
+    .getPropertyValue("top"));
+
+    const blockLeft = 
+    parseInt(window.getComputedStyle(block)
+    .getPropertyValue("left"));
+
+    if (blockLeft <20 && blockLeft >0 && characterTop >=130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        
+        alert("You LOST!");
+    }
+
+}, 10);
